dd5400e4a06c42b97c1366049fe3e5fa4ee208af
aungaunglwin99 <aungaunglwin2061999@gmail.com>
2025-12-14 19:05:02 +0900

    AddBTGotoLoginSystem

--- app/src/main/java/com/example/middleexam/Login.kt
+++ app/src/main/java/com/example/middleexam/Login.kt
@@ -7,11 +7,13 @@ import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loginsystem.DatabaseHelper
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 
 class Login : AppCompatActivity() {
     lateinit var toolbar : Toolbar
     lateinit var db: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -20,6 +22,7 @@ class Login : AppCompatActivity() {
         val etUser = findViewById<TextInputEditText>(R.id.etUser)
         val etPass = findViewById<TextInputEditText>(R.id.etPass)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnGoRegister = findViewById<Button>(R.id.btnGoRegister)
 
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -36,7 +39,12 @@ class Login : AppCompatActivity() {
                 toast("Invalid login")
             }
         }
+        btnGoRegister.setOnClickListener {
+            startActivity(Intent(this, LoginSystem::class.java))
+            finish()
+        }
     }
+
 
     private fun toast(msg: String) =
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
